ec5822ca55477cb0cbcc53a05ae6ead640128b6b
Dinesh <dinesh5.conestoga@gmail.com>
2025-03-26 21:30:55 -0400

    Added Report view info in ui and report calidator in backend

---
+++ backend/controllers/report/reportValidator.js
@@ -0,0 +1,15 @@
+const { lowercase, uppercase } = require("../../helpers/typography");
+const { sendResponse, httpCodes } = require("../../helpers/utility");
+
+module.exports = {
+  //   async validateSubmitQuiz(req, res, next) {
+  //     try {
+  //       const {} = req.body;
+  //       next();
+  //     } catch (e) {
+  //       return sendResponse(res, httpCodes.BAD_REQUEST, {
+  //         message: e,
+  //       });
+  //     }
+  //   },
+};
